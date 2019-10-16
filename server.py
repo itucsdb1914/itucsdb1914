9337cf39f1e7cba832e912df8c1f38f6beebe41f
Damla Nisa CEVIK <damlanisa.cevik@gmail.com>
2019-10-16 21:30:27 +0300

    Initialize home.html

--- server.py
+++ server.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/home")
 def home_page():
-    return "Iwent start!"
+    return render_template('home.html')
 
 
 if __name__ == "__main__":
