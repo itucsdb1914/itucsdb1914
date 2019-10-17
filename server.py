cb8c1cb0065b8780e2238609e341ea952de28f02
Enes <kuycak@itu.edu.tr>
2019-10-17 16:08:53 +0300

    Created layout template and extended html pages

--- server.py
+++ server.py
@@ -4,11 +4,35 @@ from flask import render_template
 
 app = Flask(__name__)
 
+
+example_event = [
+    {
+        'eventName': 'Joker',
+        'eventDate': '20.10.2019',
+        'eventPlace': 'Cinemaximum',
+        'eventPrice': '20'
+    },
+    {
+        'eventName': 'Batman',
+        'eventDate': '20.11.2029',
+        'eventPlace': 'Cinepink',
+        'eventPrice': '10'
+    }
+]
+
+
+
+
 @app.route("/")
 @app.route("/home")
 def home_page():
-    return render_template('home.html')
+    return render_template('home.html', events= example_event)
+
+
+@app.route("/about")
+def about_page():
+    return render_template('about.html')
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
