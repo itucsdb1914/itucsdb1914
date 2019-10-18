00866145c4dcba6c1fb2710f0450ef996801577c
ituitis-sekerbu <brkskr01@gmail.com>
2019-10-18 22:47:17 +0300

    Revert "Delete Iwent folder"
    This reverts commit 9bd18380d96474cb425d4157a0aef259857d5931.

--- Iwent/server.py
+++ Iwent/server.py
@@ -50,7 +50,10 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
             return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 
