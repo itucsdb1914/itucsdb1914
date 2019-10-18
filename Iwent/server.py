e4aa69c88d7d1221be6cb75687d18262c9e4a384
ituitis-sekerbu <brkskr01@gmail.com>
2019-10-18 21:15:44 +0300

    Adding register and login routes

--- Iwent/server.py
+++ Iwent/server.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask import Flask,redirect,flash,url_for
 from flask import render_template
-
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '65ed7f070f0943200fa33cdb18faf156'
+
 
 
 example_event = [
@@ -32,6 +35,27 @@ def home_page():
 @app.route("/about")
 def about_page():
     return render_template('about.html')
+
+@app.route("/register", methods=['GET', 'POST'])
+
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home_page'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == "__main__":
