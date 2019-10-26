8653160c89eec57405110d391e2259ed656d2035
Damla Nisa CEVIK <damlanisa.cevik@gmail.com>
2019-10-26 14:03:38 +0300

    Define template_folder in server.py

--- Iwent/server.py
+++ Iwent/server.py
@@ -2,7 +2,7 @@ from flask import Flask,redirect,flash,url_for
 from flask import render_template
 from forms import RegistrationForm, LoginForm
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 app.config['SECRET_KEY'] = '65ed7f070f0943200fa33cdb18faf156'
 
