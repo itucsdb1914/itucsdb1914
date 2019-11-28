c21f18bcb2f400648760cb1b44febd0694808209
ituitis-sekerbu <brkskr01@gmail.com>
2019-11-28 23:12:17 +0300

    Adding flask_login and flask_bcrypt libraries

--- Iwent/__init__.py
+++ Iwent/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '65ed7f070f0943200fa33cdb18faf156'
-
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 from Iwent import routes
