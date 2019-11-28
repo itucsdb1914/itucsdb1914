ec52691d2ce7c2145c03d7bace0368c4c43f05ac
ituitis-sekerbu <brkskr01@gmail.com>
2019-11-28 23:15:29 +0300

    Creating User table functions

--- Iwent/tables.py
+++ Iwent/tables.py
@@ -3,35 +3,29 @@ import os
 import io
 from flask import current_app as app
 from flask_login import UserMixin
+from Iwent import login_manager
 
+@login_manager.user_loader
+def load_user(user_id):
+    users = User().retrieve('*', f"user_id = {user_id}")
+    if users:
+        user = users[0]
+    else:
+        user = None
+    return user
 
 class BaseModel:
     fields = []
 
-    def __init__(self, url=None, *args, **kwargs):
+    def __init__(self, url=None):
         if url:
             self.connection_url = url
         else:
             self.connection_url = app.config["DATABASE_URI"]
 
-        for required_field in self.required_fields:
-            if required_field not in kwargs:
-                assert False, (f"{required_field} is required.")
-            value = kwargs[required_field]
-            setattr(self, required_field, value)
-
-        for optional_field in self.optional_fields:
-            if optional_field in kwargs:
-                value = kwargs[optional_field]
-                setattr(self, optional_field, value)
 
     def create(self):
-        table_name = self.__name__ + "S"
-        values = (getattr(self, field) for field in self.required_fields + self.optional_fields if hasattr(self, field))
-        statement = f"""
-                INSERT INTO {table_name} ({",".join(self.fields)}) values
-                ({",".join(["%s" for field in range(len(self.fields))])})"""
-        self.execute(statement, values)
+        pass
 
     def update(self):
         pass
@@ -52,20 +46,43 @@ class BaseModel:
         return response
 
     def join(self, queryKey, condition=None, variables=None):
-        statement = f"""
-        select {queryKey} from users"""
-        if (condition):
-            statement += f"""
-            where {condition}
-            """
-        query = self.execute(statement, variables, fetch=True)
-        return query
-
+        pass
 
 class User(BaseModel, UserMixin):
-    required_fields = ["user_id", "name", "username", "email", "password", "date_created", "date_updated"]
-    optional_fields = []
 
+    def __init__(self, user_id=None, username=None,
+                 email=None, password=None):
+        super(User, self).__init__()
+        self.user_id = user_id
+        self.username = username
+        self.email = email
+        self.password = password
+        print(user_id,username,email,password)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
+
+    def create(self):
+        statement = """
+        insert into users (username, email, password)
+        values (%s, %s, %s)
+        """
+        self.execute(statement, ( self.username, self.password,self.email))
+
+    def update(self):
+       pass
+
+    def retrieve(self, queryKey, condition=None, variables=None):
+       pass
+
+    def delete(self):
+        statement = """
+        temp
+        """
+        self.execute(statement)
+
+    def get_id(self):
+        return str(self.user_id)
 
 class Event(BaseModel):
     required_fields = ["event_id", "name", "place", "date", "time", "organization", "date_created", "date_updated"]
