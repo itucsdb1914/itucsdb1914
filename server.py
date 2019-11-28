f65d3c17507f3fb2b72e215079f9b6945cc8c907
Damla Nisa CEVIK <damlanisa.cevik@gmail.com>
2019-11-28 23:39:10 +0300

    Update requirements, Change DATABASE_URI config

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 from Iwent import app
 import os
 
-app.config["DATABASE_URI"] = "postgres://jhnpckexbslqcy:ce78071326738d66de8bc98fc93c0c54b43dd4f9eeda2c7345d6730f9b6153bf@ec2-176-34-183-20.eu-west-1.compute.amazonaws.com:5432/dbbdt1ie3k7s0f"
-
+app.config["DATABASE_URI"] = os.getenv("DATABASE_URL")
 if __name__ == "__main__":
     app.run()
